fea1515db5dd572a40271353e9db59164bef38ca
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-08-23 15:50:30 +0430

    Bootstrap app

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import config from 'config';
+import express from 'express';
+
+const HOST = config.get<string>('host');
+const PORT = config.get<number>('port');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+    console.log(`Server is listening ${HOST}:${PORT}.`);
+});
